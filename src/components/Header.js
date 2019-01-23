9d9cfecbf19fb8aae460903277fc7744b238599a
Jmetters <jmetters@ymail.com>
2019-01-23 17:25:19 -0600

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = props => (
 
 <div className="backdrop">
-<h2 className="knockout_text">Recipes</h2>
+<h2 className="knockout_text"><span className="color_blank">Rec</span>ipes</h2>
 </div>
 
 	);
